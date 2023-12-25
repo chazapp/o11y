9b9b8eeb4156642c1e4666ede77163fc5ffc90fd
chaz <shad@chaz.pro>
2023-12-25 16:10:48 +0100

    ci trigger

--- charts.tf
+++ charts.tf
@@ -9,6 +9,7 @@ resource "helm_release" "kube-prometheus-stack" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
   version    = "51.8.1"
+  
   namespace  = var.tools_namespace
 
   values = [
