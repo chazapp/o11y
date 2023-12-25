1d0f493e26e59fa8ea5a23455dfbd970fcc795dc
chaz <shad@chaz.pro>
2023-12-25 17:46:05 +0100

    apps/wallfront: :fire: removed jest tests from

--- apps/wall_front/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App apiUrl={"http://foo"}/>);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
