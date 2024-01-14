02e32004298845a5bf7f6ec8f4e2ae19db470d66
chaz <shad@chaz.pro>
2024-01-14 13:11:33 +0100

    :fire: removed everything from gh-pages

--- apps/wall_api/build-publish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-docker build -t ghcr.io/chazapp/o11y/wall_api:$1 .
-docker push ghcr.io/chazapp/o11y/wall_api:$1
--- apps/wall_front/src/Canvas.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useRef, useEffect } from 'react'
-
-const Canvas = (props: any) => {
-}
-
-export default Canvas
